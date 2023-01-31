e9fce4028fdac3cb190a39391e213c73396812b4
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-01-31 16:58:35 +0530

    Score texts

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -11,8 +11,9 @@ public class Ball : MonoBehaviour
     private void Start()
     {
         ResetPosition();
+        addStartingForce();
     }
-    private void addStartingForce()
+    public void addStartingForce()
     {
         float x = Random.value<0.5f? -1.0f: 1.0f;
         float y = Random.value < 0.5f? Random.Range(-1.0f, -0.5f): Random.Range(0.5f, 1.0f) ;
@@ -27,7 +28,5 @@ public class Ball : MonoBehaviour
     {
         _rigidbody.position = Vector3.zero;
         _rigidbody.velocity= Vector3.zero;
-
-        addStartingForce();
     }
 }
--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public float playerScore;
     public float computerScore;
     public Ball ball;
-
+    public Text computerText;
+    public Text playerText;
+    
     public void PlayerScores()
     {
         playerScore++;
+        playerText.text = playerScore.ToString();
         ball.ResetPosition();
+        ball.addStartingForce();
     }
     public void ComputerScores()
     {
         computerScore++;
+        computerText.text = computerScore.ToString();
         ball.ResetPosition();
+        ball.addStartingForce();
     }
 
 
